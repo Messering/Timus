0032e04273f67ac137f1fac0f817df9188a30e6d
Messering <messering@yandex.ru>
2016-01-08 18:44:16 +0200

    Timus task 1001

---
+++ Timus1001/Timus1001/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timus1001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) {
+                try
+                {
+                    Console.WriteLine("Enter number or if exit enter Z: ");
+                    string S = Console.ReadLine();
+                    if (S == "Z" || S== "z") break;
+                    else {
+                        double a = Double.Parse(S);
+                        if (a >= 0 || a <= Math.Pow(a, 18))
+                        {
+                            Console.WriteLine("Sqrt(a)={0:}", Math.Round(Math.Sqrt(a), 4));
+                        }
+                        else {
+                            Console.WriteLine("Number exit period");
+                        }
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Is not number: {0}",e);
+                    throw;
+                }
+                
+            }
+
+        }
+    }
+}
