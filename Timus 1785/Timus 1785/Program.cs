63e9f035a9b9439a79eafb96bff65ea01bedd914
Messering <messering@yandex.ru>
2016-01-08 22:55:10 +0200

    Timus task 1785

---
+++ Timus 1785/Timus 1785/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timus_1785
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number monster: ");
+            int numberMonster = int.Parse(Console.ReadLine());
+            if (numberMonster >= 1 && numberMonster <= 2000) {
+                if(numberMonster>=1 && numberMonster <= 4)
+                {
+                    Console.WriteLine("few");
+                }
+                if(numberMonster >= 5 && numberMonster <= 9)
+                {
+                    Console.WriteLine("several");
+                }
+                if (numberMonster >= 10 && numberMonster <= 19)
+                {
+                    Console.WriteLine("pack");
+                }
+                if (numberMonster >= 20 && numberMonster <= 49)
+                {
+                    Console.WriteLine("lots");
+                }
+                if (numberMonster >= 50 && numberMonster <= 99)
+                {
+                    Console.WriteLine("horde");
+                }
+                if (numberMonster >= 100 && numberMonster <= 249)
+                {
+                    Console.WriteLine("throng");
+                }
+                if (numberMonster >= 250 && numberMonster <= 499)
+                {
+                    Console.WriteLine("swarm");
+                }
+                if (numberMonster >= 500 && numberMonster <= 999)
+                {
+                    Console.WriteLine("zounds");
+                }
+                if(numberMonster>=1000)
+                {
+                    Console.WriteLine("legion");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Number monster exit period");
+            }
+        }
+    }
+}
---
+++ Timus 1785/Timus 1785/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Timus 1785")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Timus 1785")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("aa732d5d-abf8-42ac-8c22-181eb613b811")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
