9858d0692b499bf76ce711790f131247cf4f3c22
Messering <messering@yandex.ru>
2016-01-09 00:08:53 +0200

    Timus task 1409

---
+++ Timus1409/Timus1409/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timus1409
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Harry:");
+            int numberHarry = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Larry:");
+            int numberLarry = int.Parse(Console.ReadLine());
+            int numberBank = (numberHarry + numberLarry) - 1;
+            Console.WriteLine("Result Harry- {0} and Larry- {1}",numberBank-numberHarry,numberBank-numberLarry);
+            Console.ReadKey();
+        }
+    }
+}
