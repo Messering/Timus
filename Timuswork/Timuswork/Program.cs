33f17fbb52f30c20ec2a61ca04b3e7c16ff52304
Messering <messering@yandex.ru>
2016-01-08 18:02:44 +0200

    Timus task 1000

---
+++ Timuswork/Timuswork/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timuswork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter b: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("a+b={0}",a+b);
+            Console.ReadKey();
+        }
+    }
+}
