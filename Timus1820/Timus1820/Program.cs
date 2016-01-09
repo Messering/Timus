0f0525d6e9e8f59a92646bed3bf6813f6f5793c7
Messering <messering@yandex.ru>
2016-01-09 16:30:03 +0200

    Timus task 1820 (no)

---
+++ Timus1820/Timus1820/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timus1820
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] date = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(i => double.Parse(i)).ToArray<double>();
+
+            if ((date[0] * 2) % date[1] != 0)
+                Console.WriteLine((date[0] * 2) / date[0] + 1);
+            else
+                Console.WriteLine((date[0] * 2) / date[1]);
+
+            Console.ReadKey();
+        }
+    }
+}
