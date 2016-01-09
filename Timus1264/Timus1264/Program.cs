b489259ee995232d8fbeb6695492859bf21df564
Messering <messering@yandex.ru>
2016-01-09 17:19:33 +0200

    Timus task 1264

---
+++ Timus1264/Timus1264/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timus1264
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] date = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(i => int.Parse(i)).ToArray<int>();
+            Console.WriteLine(date[0]*(date[1]+1));
+            Console.ReadKey();
+        }
+    }
+}
