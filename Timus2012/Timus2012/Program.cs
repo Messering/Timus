a4550b25403c3a338d09654a56aa85461b047984
Messering <messering@yandex.ru>
2016-01-09 15:50:16 +0200

    Timus task 2012

---
+++ Timus2012/Timus2012/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timus2012
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int f = int.Parse(Console.ReadLine());
+            if (f >= 1 && f <= 11) {
+                if (4 * 60 >= (12 - f) * 45) { Console.WriteLine("YES"); }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
