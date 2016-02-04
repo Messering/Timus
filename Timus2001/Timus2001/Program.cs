129bc1572a21887632eff7c3c0fb33d16b288732
Messering <messering@yandex.ru>
2016-02-04 16:25:53 +0200

    Timus task 2001 completed

---
+++ Timus2001/Timus2001/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timus2001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] date1 = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(i => int.Parse(i)).ToArray<int>();
+            int[] date2 = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(i => int.Parse(i)).ToArray<int>();
+            int[] date3 = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(i => int.Parse(i)).ToArray<int>();
+            Console.WriteLine((date1[0]-date3[0])+" "+ (date1[1]-date2[1]));
+        }
+    }
+}
